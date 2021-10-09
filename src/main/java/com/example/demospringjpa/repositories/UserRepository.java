25f96f9790b9ea38ce2a7aae6a7c2d658c4930f6
Martins <vinicius.martins@capgemini.com>
2021-10-09 19:24:45 -0300

    JPA repository, dependency, database seeding

---
+++ src/main/java/com/example/demospringjpa/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.demospringjpa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demospringjpa.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+	
+
+}
